0827e97039479d745871f9ebd253fbe4c6841bd7
GROSS Anthony <anthony.k.gross@gmail.com>
2015-09-07 15:31:04 +0200

    Security first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo -n "anthonykgross/stream123456789" | openssl dgst -md5 -binary | openssl enc -base64 | tr '+/' '-_' | tr -d '='
+
 mkdir nginx
 cd nginx
 
@@ -10,7 +12,7 @@ wget http://nginx.org/download/nginx-1.4.3.tar.gz
 tar zxpvf nginx-1.4.3.tar.gz
 cd nginx-1.4.3
 
-./configure --add-module=/nginx/nginx-rtmp-module/ --with-http_ssl_module --prefix=/usr/local/nginx-streaming
+./configure --add-module=/nginx/nginx-rtmp-module/ --with-http_ssl_module --with-http_secure_link_module --prefix=/usr/local/nginx-streaming
 make
 make install
 
