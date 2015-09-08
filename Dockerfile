d0c5a0c7f2edacb609db32f69f3d122307789567
GROSS Anthony <anthony.k.gross@gmail.com>
2015-09-08 12:02:52 +0200

    Image fully customizable like docker-composer

--- Dockerfile
+++ Dockerfile
@@ -8,15 +8,48 @@ RUN echo "deb-src http://www.deb-multimedia.org jessie main non-free" >> /etc/ap
 RUN apt-get update -y && \
 	apt-get upgrade -y && \
 	apt-get install -y --force-yes deb-multimedia-keyring supervisor wget
-
 RUN apt-get install -y --force-yes git gcc make libpcre3-dev libssl-dev ffmpeg --fix-missing
-
 RUN rm -rf /var/lib/apt/lists/* && apt-get autoremove -y --purge
 
-EXPOSE 80
-EXPOSE 443
+ENV YOUTUBE_URL rtmp://a.rtmp.youtube.com/live2
+ENV YOUTUBE_STREAMKEY NULL
+
+ENV TWITCH_URL rtmp://live-cdg.twitch.tv/app
+ENV TWITCH_STREAMKEY NULL
+
+ENV HITBOX_URL rtmp://live.hitbox.tv/push
+ENV HITBOX_STREAMKEY NULL
+
+ENV DAILYMOTION_URL rtmp://publish.dailymotion.com/publish-dm
+ENV DAILYMOTION_STREAMKEY NULL
+
+ENV YOUR_IP YOUR_IP
+ENV PRIVATE_KEY anthonykgross
+ENV STREAM_SPECIFIER hd720
+ENV URL_TRANSCODE transcode
+ENV URL_LIVE live
+
+
+RUN mkdir /conf
+ADD ./conf /conf
+
+RUN mkdir nginx && \
+	cd nginx && \
+	git clone https://github.com/arut/nginx-rtmp-module && \
+	wget http://nginx.org/download/nginx-1.4.3.tar.gz && \
+	tar zxpvf nginx-1.4.3.tar.gz && \
+	cd nginx-1.4.3 && \
+	./configure --add-module=/nginx/nginx-rtmp-module/ --with-http_ssl_module --with-http_secure_link_module --prefix=/usr/local/nginx-streaming && \
+    make && \
+	make install
+
+RUN cp /conf/nginx/nginx.conf /usr/local/nginx-streaming/conf/nginx.conf -f
+RUN cp /conf/supervisor/conf.d/supervisor.conf /etc/supervisor/conf.d/supervisor.conf -f
 
 ADD entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
+
+EXPOSE 8080
+EXPOSE 1935
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,71 @@
 #!/bin/bash
 set -e
 
-#echo -n "anthonykgross/stream123456789" | openssl dgst -md5 -binary | openssl enc -base64 | tr '+/' '-_' | tr -d '='
+sed -i -e "s,<url_transcode>,$URL_TRANSCODE,g" /usr/local/nginx-streaming/conf/nginx.conf
+sed -i -e "s,<url_live>,$URL_LIVE,g" /usr/local/nginx-streaming/conf/nginx.conf
+sed -i -e "s,<stream_specifier>,$STREAM_SPECIFIER,g" /usr/local/nginx-streaming/conf/nginx.conf
+sed -i -e "s,<private_key>,$PRIVATE_KEY,g" /usr/local/nginx-streaming/conf/nginx.conf
 
-mkdir nginx
-cd nginx
+sed -i -e "s,<twitch_url>,$TWITCH_URL,g" /usr/local/nginx-streaming/conf/nginx.conf
+sed -i -e "s,<hitbox_url>,$HITBOX_URL,g" /usr/local/nginx-streaming/conf/nginx.conf
+sed -i -e "s,<youtube_url>,$YOUTUBE_URL,g" /usr/local/nginx-streaming/conf/nginx.conf
+sed -i -e "s,<dailymotion_url>,$DAILYMOTION_URL,g" /usr/local/nginx-streaming/conf/nginx.conf
 
-git clone https://github.com/arut/nginx-rtmp-module
+echo "==============================================================================================="
+echo "="
+if [[ $TWITCH_STREAMKEY != "NULL" ]]
+then
+    echo "= TWITCH_STREAMKEY      : $TWITCH_STREAMKEY"
+    sed -i -e "s,<twitch_active>, ,g" /usr/local/nginx-streaming/conf/nginx.conf
+    sed -i -e "s,<twitch_streamkey>,$TWITCH_STREAMKEY,g" /usr/local/nginx-streaming/conf/nginx.conf
+    
+else
+	echo "= TWITCH_STREAMKEY      : NOT FOUND!"
+	sed -i -e "s,<twitch_active>,#,g" /usr/local/nginx-streaming/conf/nginx.conf
+fi
 
-wget http://nginx.org/download/nginx-1.4.3.tar.gz
-tar zxpvf nginx-1.4.3.tar.gz
-cd nginx-1.4.3
+if [[ $HITBOX_STREAMKEY != "NULL" ]]
+then
+    echo "= HITBOX_STREAMKEY      : $HITBOX_STREAMKEY"
+    sed -i -e "s,<hitbox_active>, ,g" /usr/local/nginx-streaming/conf/nginx.conf
+    sed -i -e "s,<hitbox_streamkey>,$HITBOX_STREAMKEY,g" /usr/local/nginx-streaming/conf/nginx.conf
+    
+else
+	echo "= HITBOX_STREAMKEY      : NOT FOUND!"
+	sed -i -e "s,<hitbox_active>,#,g" /usr/local/nginx-streaming/conf/nginx.conf
+fi
 
-./configure --add-module=/nginx/nginx-rtmp-module/ --with-http_ssl_module --with-http_secure_link_module --prefix=/usr/local/nginx-streaming
-make
-make install
+if [[ $YOUTUBE_STREAMKEY != "NULL" ]]
+then
+    echo "= YOUTUBE_STREAMKEY     : $YOUTUBE_STREAMKEY"
+    sed -i -e "s,<youtube_active>, ,g" /usr/local/nginx-streaming/conf/nginx.conf
+    sed -i -e "s,<youtube_streamkey>,$YOUTUBE_STREAMKEY,g" /usr/local/nginx-streaming/conf/nginx.conf
+    
+else
+	echo "= YOUTUBE_STREAMKEY     : NOT FOUND!"
+	sed -i -e "s,<youtube_active>,#,g" /usr/local/nginx-streaming/conf/nginx.conf
+fi
 
-cp /usr/local/nginx-streaming/conf/new-nginx.conf /usr/local/nginx-streaming/conf/nginx.conf -f
+if [[ $DAILYMOTION_STREAMKEY != "NULL" ]]
+then
+    echo "= DAILYMOTION_STREAMKEY : $DAILYMOTION_STREAMKEY"
+    sed -i -e "s,<dailymotion_active>, ,g" /usr/local/nginx-streaming/conf/nginx.conf
+    sed -i -e "s,<dailymotion_streamkey>,$DAILYMOTION_STREAMKEY,g" /usr/local/nginx-streaming/conf/nginx.conf
+    
+else
+	echo "= DAILYMOTION_STREAMKEY : NOT FOUND!"
+	sed -i -e "s,<dailymotion_active>,#,g" /usr/local/nginx-streaming/conf/nginx.conf
+fi
 
+today=$(date +%s)
+expire=$(($today+3600))
+token=$(echo -n "$PRIVATE_KEY/stream1370781049" | openssl dgst -md5 -binary | 
+         openssl enc -base64 | tr '+/' '-_' | tr -d '=')
+
+
+echo "="
+echo "= URL live      = rtmp://$YOUR_IP:1935/$URL_LIVE/stream?e=$expire&st=$token"
+echo "= URL transcode = rtmp://$YOUR_IP:1935/$URL_TRANSCODE/stream?e=$expire&st=$token"
+echo "="
+echo "==============================================================================================="
 supervisord
